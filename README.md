4212b356b7e15983f842a44857f6d9bcddead1ea
HCLonely <h1606051253@gmail.com>
2020-02-10 12:37:39 +0800

    Update usage

--- README.md
+++ README.md
@@ -41,13 +41,16 @@ bangumi:
 2. 输入`hexo new page bangumis`
 3. 你会找到`source/bangumis/index.md`这个文件
 4. 修改这个文件，添加`type: "bangumis"`：
-```markdown
----
-title: bangumis
-date: 2018-01-05 00:00:00
-type: "bangumis"
----
-```
+
+    ```markdown
+    ---
+    title: bangumis
+    date: 2018-01-05 00:00:00
+    type: "bangumis"
+    ---
+    ```
+
+5. 防止请求次数过多插件不再获取番剧数据，所以请根据自己的需要在`hexo generate`或`hexo deploy`之前使用`hexo bangumi update`命令更新番剧数据！
 
 ## 获取uid
 
--- index.js
+++ index.js
@@ -2,34 +2,93 @@
 'use strict';
 const fs = require('hexo-fs');
 const path = require('path');
+const axios = require("axios");
 const log = require('hexo-log')({
   debug: false,
   silent: false
 });
 
+
 hexo.extend.generator.register('bangumis', function (locals) {
-  if (!this.config.bangumi) {
+  if (!this.config.bangumi || !this.config.bangumi.enable) {
     return;
   }
   return require('./lib/bangumi-generator').call(this, locals);
 });
-
-hexo.extend.console.register('bangumis', 'Generate pages from bangumi', function (args) {
-  //Register route
-  hexo.extend.generator.register("bangumis", require('./lib/bangumi-generator'));
-
-  let self = this;
-  let publicDir = self.public_dir;
-
-  //Generate files
-  self.load().then(function () {
-    if (!fs.existsSync(publicDir)) {
-      fs.mkdirSync(publicDir);
-    }
-    const id = "bangumis/index.html";
-    self.route.get(id) && self.route.get(id)._data().then(function (contents) {
-      fs.writeFile(path.join(publicDir, id), contents);
-      log.info("Generated: %s", id);
-    });
-  });
+hexo.extend.console.register('bangumi update', 'Update bilibili bangumis data', function (args) {
+  if (!this.config.bangumi || !this.config.bangumi.enable) {
+    log.info("Please add config to _config.yml");
+    return;
+  }
+  if (!this.config.bangumi.vmid) {
+    log.info("Please add vmid to _config.yml");
+    return;
+  }
+  saveBangumiData(this.config.bangumi.vmid);
 });
+
+async function getBangumiPage(vmid, status) {
+  let response = await axios.get(`https://api.bilibili.com/x/space/bangumi/follow/list?type=1&follow_status=${status}&vmid=${vmid}&ps=1&pn=1`)
+  return Math.ceil(response.data.data.total / 50) + 1;
+}
+async function getBangumi(vmid, status, pn) {
+  let response = await axios.get(`https://api.bilibili.com/x/space/bangumi/follow/list?type=1&follow_status=${status}&vmid=${vmid}&ps=50&pn=${pn}`)
+  let $data = [];
+  if (response.data.code === 0) {
+    let data = response.data.data;
+    let list = data.list;
+    for (let bangumi of list) {
+      $data.push({
+        title: bangumi.title,
+        type: bangumi.season_type_name,
+        area: bangumi.areas[0].name,
+        cover: bangumi.cover + "@220w_280h.webp",
+        totalCount: total(bangumi.total_count),
+        id: bangumi.media_id,
+        follow: count(bangumi.stat.follow),
+        view: count(bangumi.stat.view),
+        danmaku: count(bangumi.stat.danmaku),
+        coin: count(bangumi.stat.coin),
+        score: bangumi.rating ? bangumi.rating.score : "暂无评分",
+        des: bangumi.evaluate
+      });
+    }
+    return $data;
+  }
+}
+function count(e) {
+  return e > 10000 && e < 100000000 ? `${(e / 10000).toFixed(1)} 万` : e > 100000000 ? `${(e / 100000000).toFixed(1)} 亿` : e;
+}
+function total(e) {
+  return e === -1 ? `未完结` : `全${e}话`;
+}
+async function biliBangumi(vmid, status) {
+  let page = await getBangumiPage(vmid, status);
+  let list = [];
+  for (let i = 1; i < page; i++) {
+    let data = await getBangumi(vmid, status, i);
+    list.push(...data);
+  }
+  return list;
+}
+async function saveBangumiData(vmid) {
+  log.info("Getting bilibili bangumis, please wait...");
+  let startTime = new Date().getTime();
+  let wantWatch = await biliBangumi(vmid, 1);
+  let watching = await biliBangumi(vmid, 2);
+  let watched = await biliBangumi(vmid, 3);
+  let endTime = new Date().getTime();
+  log.info(wantWatch.length + watching.length + watched.length + ' bangumis have been loaded in ' + (endTime - startTime) + " ms");
+  let bangumis = { wantWatch, watching, watched };
+  if (!fs.existsSync(path.join(__dirname, "/data/"))) {
+    fs.mkdirsSync(path.join(__dirname, "/data/"));
+  }
+  fs.writeFile(path.join(__dirname, "/data/bangumis.json"), JSON.stringify(bangumis), err => {
+    if (err) {
+      log.info("Failed to write data to bangumis.json");
+      console.log(err);
+    } else {
+      log.info("Bilibili bangumis data saved");
+    }
+  });
+}
