5926ee83e083833bcafd39ff82785412d43873aa
HCLonely <h1606051253@gmail.com>
2023-06-13 10:33:16 +0800

    修复使用bangumi源时套用bili模板;优化Pjax兼容性(#311)

--- src/lib/bangumi-generator.js
+++ src/lib/bangumi-generator.js
@@ -79,7 +79,7 @@ module.exports = async function (locals, type = 'bangumi') {
     showMyComment: config[type].showMyComment ?? false,
     pagination: config[type].pagination ?? false,
     theme: fs.existsSync(path.join(__dirname, `templates/theme/${config.theme}.min.css`)) ? config.theme : null,
-    ejsTemplate: fs.readFileSync(path.join(__dirname, `templates/${config[type].source === 'bgm' ? 'bgm' : 'bili'}-template.ejs`)).toString()
+    ejsTemplate: fs.readFileSync(path.join(__dirname, `templates/${config[type].source === 'bili' ? 'bili' : 'bgm'}-template.ejs`)).toString()
       .replace('class="bangumi-item"', 'class="bangumi-item bangumi-hide"'),
     wantWatch,
     watched,
